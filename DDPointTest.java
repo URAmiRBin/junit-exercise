e79ee605cafd35f4ecaa1e7f16d55131fa539d1a
Amir Bin <ur.amirbin@gmail.com>
2021-05-15 21:49:41 +0430

    Add theories for equality

--- DDPointTest.java
+++ DDPointTest.java
@@ -1,37 +1,48 @@
-import java.util.Arrays;
-import java.util.Collection;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-import static org.junit.Assert.*;
-import org.junit.*;
-
-@RunWith (Parameterized.class)
+@RunWith(Theories.class)
 public class DDPointTest {
-    public Point a;
-    public Point b;
-    public boolean equals;
+    @DataPoints
+    public static Object[] p = {
+        new Point(2, 3),
+        new Point(3, 4),
+        new Point(1, 1),
+        null,
+        2
+    };
 
-    public DDPointTest(Point a, Point b, boolean result) {
-        this.a = a;
-        this.b = b;
-        this.equals = result;
+    @Theory
+    public void ReflexiveProperties(Point a) {
+        assumeTrue(a != null);
+        
+        assertTrue(a.equals(a));
     }
 
-    @Parameters
-    public static Collection<Object[]> equalValues() {
-        return Arrays.asList(new Object[][] {
-            {new Point(2, 3), new Point(3, 3), false},
-            {new Point(2, 3), new Point(2, 2), false},
-            {new Point(2, 3), new Point(3, 2), false},
-            {new Point(2, 3), new Point(2, 3), true},
-        });
+    @Theory
+    public void symmetricProperty(Point a, Point b) {
+        assumeTrue(a != null);
+        assumeTrue(b != null);
+
+        assertEquals(a.equals(b), b.equals(a));
     }
 
-    @Test
-    public void equalsTest() {
-        assertTrue("Equals Test", equals == a.equals(b));
+    @Theory
+    public void transitiveProperty(Point a, Point b, Point c) {
+        assumeTrue(a != null);
+        assumeTrue(b != null);
+        assumeTrue(c != null);
+
+        assumeTrue(a.equals(b));
+        assumeTrue(b.equals(c));
+        assertTrue(a.equals(c));
     }
+
+
 }
