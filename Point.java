838604ba4e566430bf63ff861420b3762ff61552
Amir Bin <ur.amirbin@gmail.com>
2021-05-15 21:37:20 +0430

    Override Point equals method

---
+++ Point.java
@@ -0,0 +1,20 @@
+public class Point{
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (o == this) return true;
+
+        if (!(o instanceof Point)) return false;
+
+        Point p = (Point)o;
+        
+        return (this.x == p.x) && (this.y == p.y);
+    }
+
+}
